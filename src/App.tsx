dec65714fb588c118ef012a7b675e131fa661b07
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-11 15:24:41 +0530

    hello

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App () {
+  return (
+    <>
+    <div className='text-5xl font-bold '>Minimilist vite,react,typescript starrter pack</div>
+    </>
+  )
+}
+
+export default App
